f31bb4d7f5e37b4b02f45dab56d8dfee38676698
Luiz Silva <luiz.gsilva42@gmail.com>
2021-01-05 14:14:40 -0300

    saga

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,6 +1,17 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, select, takeLatest } from 'redux-saga/effects';
+import { IState } from '../..';
+import { addProductToCart } from './actions';
 
-function checkProductStock() {
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>
+
+function* checkProductStock({payload}: CheckProductStockRequest) {
+  const { product } = payload;
+  
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
+  })
+
+  console.log(currentQuantity)
 	console.log('ADD to card')
 }
 
